0470c90719b07bdf04613adf0f715250a95c9fe4
Sumuditha-Janith <sumuditha356@gmail.com>
2025-09-05 02:56:48 +0530

    initial commit

---
+++ src/main/java/lk/ijse/gdse71/loslibros/LoslibrosApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse71.loslibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoslibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoslibrosApplication.class, args);
+	}
+
+}
