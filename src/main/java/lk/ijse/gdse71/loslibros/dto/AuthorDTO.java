2a5e78b07bdaad1ceda62526c5ee5e6170facb58
Sumuditha-Janith <sumuditha356@gmail.com>
2025-09-05 05:36:50 +0530

    au, bo, cat DTOs

--- src/main/java/lk/ijse/gdse71/loslibros/dto/AuthorDTO.java
+++ src/main/java/lk/ijse/gdse71/loslibros/dto/AuthorDTO.java
@@ -1,4 +1,14 @@
 package lk.ijse.gdse71.loslibros.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorDTO {
+    private Long authorId;
+    private String authorName;
+    private String authorDescription;
 }
--- src/main/java/lk/ijse/gdse71/loslibros/dto/BookDTO.java
+++ src/main/java/lk/ijse/gdse71/loslibros/dto/BookDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse71.loslibros.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDTO {
+    private Long bookId;
+    private String bookTitle;
+    private Double bookPrice;
+    private Integer bookQuantity;
+    private String bookImage;
+    private AuthorDTO bookAuthor;
+    private CategoryDTO bookCategory;
 }
